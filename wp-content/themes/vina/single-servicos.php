50eee6078dab0645c65b50b79b3843e738b6f87a
Thiago Ferraz Hotts Peixoto <thiago.peixoto@groupsoftware.com.br>
2021-08-03 11:58:08 -0300

    Ajustes nos serviços e atualizações do wordpress e plugins

--- wp-content/themes/vina/single-servicos.php
+++ wp-content/themes/vina/single-servicos.php
@@ -6,9 +6,38 @@ require_once('header.php');
 <section class="conteudo">
   <div class="container">
     <div class="row d-flex justify-content-start">
-      <div class="col-10">
+      <div class="col-12 col-md-7">
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
+      </div>
+
+      <div class="col-12 col-md-5">
+        <ul class="lateral-servicos">
+          <?php
+          $args = array(
+            'post_type' => 'servicos',
+            'posts_per_page' => -1
+          );
+          $servicos = new WP_Query($args);
+          if ($servicos->have_posts()) : while ($servicos->have_posts()) : $servicos->the_post(); ?>
+
+
+              <li>
+                <a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>">
+                  <?php the_title();  ?>
+                </a>
+              </li>
+
+              <?php /*<p><?php the_excerpt(); ?></p>*/ ?>
+
+
+            <?php endwhile;
+          else : ?>
+
+            <p>Não há posts publicados</p>
+
+          <?php endif; ?>
+        </ul>
       </div>
     </div>
   </div>
