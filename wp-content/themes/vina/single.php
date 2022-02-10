c6f1dd674c62ef14b5f99d7053429bdf378eeb83
Thiago Peixoto <tfpeixoto@gmail.com>
2022-02-10 20:24:59 -0300

    correção do tamanho da fonte dos posts

--- wp-content/themes/vina/single.php
+++ wp-content/themes/vina/single.php
@@ -6,7 +6,7 @@ require_once('header.php');
 <section class="conteudo">
   <div class="container">
     <div class="row justify-content-start">
-      <div class="col-8">
+      <div class="col-12 col-md-8">
         <h1><?php the_title(); ?></h1>
         <p><strong><?php $data = get_the_date(); echo $data ?></strong></p>
         <?php the_content(); ?>
