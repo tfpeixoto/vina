851d467c5693e31111d940cdce0f126aa055e4e6
Thiago Peixoto <tfpeixoto@gmail.com>
2023-01-02 23:21:24 -0300

    update plugins

--- wp-content/plugins/seo-by-rank-math/vendor/composer/installed.php
+++ wp-content/plugins/seo-by-rank-math/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'rankmath/seo-by-rank-math',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => 'dcc3b41489a7d05b9b0711996f06cdec776c79fa',
+        'pretty_version' => '1.0.105.x-dev',
+        'version' => '1.0.105.9999999-dev',
+        'reference' => 'bd55f0382f69d3cb20c98a6624a0ddfd543230fb',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'rankmath/seo-by-rank-math' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => 'dcc3b41489a7d05b9b0711996f06cdec776c79fa',
+            'pretty_version' => '1.0.105.x-dev',
+            'version' => '1.0.105.9999999-dev',
+            'reference' => 'bd55f0382f69d3cb20c98a6624a0ddfd543230fb',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
