a8f693a3e76fb4a5039ec1f3ab44ce11df47b94e
Thiago Peixoto <tfpeixoto@gmail.com>
2023-04-10 22:47:52 -0300

    create category page

--- wp-content/themes/vina/category.php
+++ wp-content/themes/vina/category.php
@@ -3,18 +3,38 @@
 
 $estiloPagina = 'page.css';
 require_once('header.php');
+
+$category = get_category(get_query_var('cat'));
+$cat_id = $category->cat_ID;
 ?>
 
 <section class="conteudo">
   <div class="container">
     <div class="row justify-content-between">
-      <div class="col-12">
+      <div class="col-8">
         <h1>VinaVina</h1>
+      </div>
+
+      <div class="col-12 col-md-4">
+        <div class="conteudo__category">
+          <strong>Categoria:</strong> <?= get_cat_name($cat_id); ?>
+        </div>
       </div>
     </div>
 
     <div class="row d-flex justify-content-start posts">
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php
+      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+      $args = array(
+        'cat' => $cat_id,
+        'post_type' => 'post',
+        'paged' => $paged,
+        'posts_per_page' => 9
+      );
+
+      $posts = query_posts($args);
+      if (have_posts()) : while (have_posts()) : the_post(); ?>
 
           <article class="col-12 col-md-4 posts__box">
             <?php if (has_post_thumbnail()) {
