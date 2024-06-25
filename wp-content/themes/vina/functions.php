9e70703b40892ffc74fb7c8100ea2c4e5ee22eb3
Thiago Peixoto <tfpeixoto@gmail.com>
2024-06-25 20:14:38 -0300

    update error

--- wp-content/themes/vina/functions.php
+++ wp-content/themes/vina/functions.php
@@ -42,8 +42,8 @@ function vina_scripts()
 {
   wp_enqueue_style('critital', get_template_directory_uri() . '/assets/css/critical.css', array(), '1.1', 'all');
 
-  wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), '3.5.1', true);
+  // wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.7.1.min.js', array(), '3.7.1', true);
   wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '4.5.3', true);
   wp_enqueue_script('lightbox', 'https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js', array('bootstrap'), '5.3.0', true);
   wp_enqueue_script('acoes', get_template_directory_uri() . '/assets/js/critical.js', array('lightbox'), '1.0', true);
--- wp-content/themes/vina/src/js/modules/scripts.js
+++ wp-content/themes/vina/src/js/modules/scripts.js
@@ -1,3 +1,5 @@
+import * as bootstrap from 'bootstrap'
+
 $('.carousel').carousel({
   interval: 5000,
   pause: true
